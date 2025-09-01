7a19539340df54cc7478eecc490bb12f238ece3d
Holl4444 <holly.cello@gmail.com>
2025-09-01 13:13:44 +0100

    refactor: breakCamelCase

--- challenges/breakCamelCase.py
+++ challenges/breakCamelCase.py
@@ -1,21 +1,10 @@
 def breakCamelCase(string: str):
     words = string.split(' ')
     clean_array = []
-    clean_word = ''
     for word in words:
-        flag = 0
-        for i, char in enumerate(word[1:]):
-            if char == char.upper():
-                if flag > 0:
-                    clean_word = clean_word[:i + 1 + flag] + ' ' + clean_word[i + 1 + flag:]
-                else:
-                    clean_word += word[:i + 1] + ' ' + word[i + 1:]
-                flag += 1
-        if flag:
-            clean_array.append(clean_word)
-            clean_word = ''
-        else:
-            clean_array.append(''.join(word))
+        word = word[0] + ''.join([f' {char}' if char == char.upper() else char for char in word[1:]])
+        clean_array.append(word)
     return ' '.join(clean_array) if len(clean_array) > 1 else clean_array[0]
 
 print(breakCamelCase('breakCamelCase'))
+
